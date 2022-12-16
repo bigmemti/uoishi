b485f6948f854fb50efae02aea887fb0dbf80ab4
gang <67897876+miseliongang@users.noreply.github.com>
2022-12-16 05:02:35 +0330

    make setting table

--- database/migrations/2022_12_16_012614_create_settings_table.php
+++ database/migrations/2022_12_16_012614_create_settings_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('en_title');
+            $table->string('fa_title');
+            $table->string('value');
             $table->timestamps();
         });
     }
