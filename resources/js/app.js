d6c0251c31d3347234db632be48bf0ada518296c
gang <67897876+miseliongang@users.noreply.github.com>
2023-02-15 03:08:35 +0330

    change sweetalert 2 adding

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,22 @@
 import './bootstrap';
 
+import Swal from 'sweetalert2';
 import Alpine from 'alpinejs';
 
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+})
+
 window.Alpine = Alpine;
+window.Swal = Swal;
+window.Toast = Toast;
 
 Alpine.start();
