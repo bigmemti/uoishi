60ad240253253451f41fe66ed9d28ca5ee412cd9
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2023-02-19 00:53:30 +0330

    delete white space in end of index task

--- resources/views/task/index.blade.php
+++ resources/views/task/index.blade.php
@@ -33,7 +33,7 @@
     </section>
 
     <section>
-        <div>
+        <div class="py-3">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
@@ -131,7 +131,7 @@
     </section>
 
     <section>
-        <div class="mt-3 px-4">
+        <div class="py-2 px-4">
             {{$tasks->links()}}
         </div>
     </section>
