5919e8dc2e0b50168a92ab14aaa6572d18508653
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2023-02-22 01:56:02 +0330

    delete white spaces

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -19,17 +19,6 @@ class SettingController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Setting  $setting
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Setting $setting)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      *
      * @param  \App\Http\Requests\UpdateSettingRequest  $request
