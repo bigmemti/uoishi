b1dda5c42e728023eea557e56fae684b88bd0d24
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2024-06-21 18:20:36 +0330

    add application structure and set body direction

--- front-end/app.vue
+++ front-end/app.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
+<script setup>
+  useHead({ bodyAttrs: { dir: 'rtl' } });
+</script>
