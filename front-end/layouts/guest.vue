a3b691bebf97a91bcdcd3dc9d8b6d46a311e66e2
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2024-06-21 18:22:50 +0330

    add guest layout

---
+++ front-end/layouts/guest.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="font-sans text-gray-900 dark:text-gray-200 antialiased">
+    <slot />  
+  </div>
+</template>
+
+<style scoped></style>
