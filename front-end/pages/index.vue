e925894373ac4be2682a97068d5aef446c349688
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2024-06-21 18:22:27 +0330

    add initializing pages

---
+++ front-end/pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="min-h-screen flex flex-col justify-start items-center">
+        <LandingHeader />
+        <main>
+            <section>
+                <div class="flex flex-col-reverse mt-3 md:flex-row md:items-center">
+                    <div class="mt-5 px-4 md:max-w-md">
+                        <p> {{ $t('Welcome') }} </p>
+                        <div class="flex justify-center items-center space-x-3 rtl:space-x-reverse mt-4">
+                            <NuxtLink class="text-white bg-violet-600 text-xs p-3 md:text-base md:p-4 rounded-xl hover:bg-violet-500 border-2 border-transparent hover:border-violet-400" href="/dashboard">{{ $t('Dashboard') }}</NuxtLink>
+                        </div>
+                        <div class="flex justify-center items-center space-x-3 rtl:space-x-reverse mt-4">
+                            <NuxtLink class="text-white bg-violet-600 text-xs p-3 md:text-base md:p-4 rounded-xl hover:bg-violet-500 border-2 border-transparent hover:border-violet-400" to="/login">{{ $t('Log in') }}</NuxtLink>
+                            <NuxtLink class="text-white bg-violet-600 text-xs p-3 md:text-base md:p-4 rounded-xl hover:bg-violet-500 border-2 border-transparent hover:border-violet-400" href="/register">{{ $t('Register') }}</NuxtLink>
+                        </div>
+                    </div>
+                    <div class="flex justify-center md:max-w-md">
+                        <img class="w-[300px] md:w-[650px]" src="" alt="">
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer class="grow-[2] flex flex-col justify-end self-stretch">
+            <div class="bg-[#3C1787] text-center py-6 text-white">
+                <span class="" >با افتخار نیرو گرفته از  
+                    ©
+                    <a href="https://github.com/bigmemti" target="_blank">
+                        bigmemti 
+                    </a>
+                </span>
+            </div>
+        </footer>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+</style>
---
+++ front-end/pages/login.vue
@@ -0,0 +1,62 @@
+<template>
+    <authCard>
+        <template #logo>
+            <NuxtLink to="/">
+                <h1 class="banner">
+                        uoishi
+                </h1>
+            </NuxtLink>
+        </template>
+
+        <!-- Session Status -->
+        <!-- <x-auth-session-status class="mb-4" status="session('status')" /> -->
+
+        <UForm class="py-4">
+
+            <!-- Email Address -->
+            <UFormGroup :label="$t('Email')" required >
+                <UInput id="email" class="my-4" type="email" name="email" required autofocus />
+            </UFormGroup>
+            
+            
+            
+            <!-- Password -->
+            <UFormGroup :label="$t('Password')" required>
+                <UInput id="email" class="my-4" type="password" nam
+                e="password" required autofocus autocomplete="current-password" />
+            </UFormGroup> 
+
+            <!-- Remember Me -->
+            <div class="block mt-4">
+                <label for="remember_me" class="inline-flex items-center">
+                    <UCheckbox id="remember_me" type="checkbox" class="rounded" name="remember"/>
+                    <span class="ltr:ml-2 rtl:mr-2 text-sm text-gray-600">{{ $t('Remember me') }} </span>
+                </label>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ $t('Forgot your password?') }} 
+                </a>
+
+                <UButton class="ltr:ml-3 rtl:mr-3 text-xs py-2 px-4">
+                        {{ $t('Log in') }} 
+                </UButton>
+            </div>
+        </UForm>
+    </authCard>
+</template>
+<script setup>
+    definePageMeta({
+        layout : 'guest'
+    });
+</script>
+<style scoped>
+    .banner{
+        font-family : Freestyle Script;
+        font-size : 5rem;
+        text-align : center;
+        color : #3C1787;
+    }
+</style>
