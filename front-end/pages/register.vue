752ebc6ccbdf3a0043cafb5f1fbd4bf0e82614b7
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2024-07-01 20:21:35 +0330

    add register page

--- front-end/pages/register.vue
+++ front-end/pages/register.vue
@@ -1,9 +1,45 @@
 <template>
+    <AuthCard>
+        <template #logo>
+            <NuxtLink to="/">
+                <Banner />
+            </NuxtLink>
+        </template>
 
+        <UForm class="py-4">
+
+            <UFormGroup :label="$t('Name')" required >
+                <UInput id="name" class="my-4" type="name" name="name" required autofocus/>
+            </UFormGroup>
+
+            <UFormGroup :label="$t('Email')" required >
+                <UInput id="email" class="my-4" type="email" name="email" required/>
+            </UFormGroup>
+
+            <UFormGroup :label="$t('Password')" required >
+                <UInput id="password" class="my-4" type="password" name="password" required/>
+            </UFormGroup>
+
+            <UFormGroup :label="$t('Confirm Password')" required >
+                <UInput id="password_confirmation" class="my-4" type="password" name="password_confirmation" required/>
+            </UFormGroup>
+
+            <div class="flex items-center justify-end mt-4">
+                <UButton variant="link" to="/login">
+                    {{ $t('Already registered?') }} 
+                </UButton>
+
+                <UButton type="submit" class="ltr:ml-3 rtl:mr-3 text-xs py-2 px-4">
+                        {{ $t('Register') }} 
+                </UButton>
+            </div>
+        </UForm>
+    </AuthCard>
 </template>
-<script setup lang="ts">
 
+<script setup>
+    definePageMeta({
+        layout : 'guest'
+    });
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
