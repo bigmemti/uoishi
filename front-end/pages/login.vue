a33ef1a4415354b07ef277a1d05c1085317bfe9a
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2024-07-01 20:21:18 +0330

    fix banner and link component in login page

--- front-end/pages/login.vue
+++ front-end/pages/login.vue
@@ -1,32 +1,23 @@
 <template>
-    <authCard>
+    <AuthCard>
         <template #logo>
             <NuxtLink to="/">
-                <h1 class="banner">
-                        uoishi
-                </h1>
+                <Banner />
             </NuxtLink>
         </template>
 
         <!-- Session Status -->
-        <!-- <x-auth-session-status class="mb-4" status="session('status')" /> -->
 
         <UForm class="py-4">
 
-            <!-- Email Address -->
             <UFormGroup :label="$t('Email')" required >
                 <UInput id="email" class="my-4" type="email" name="email" required autofocus />
             </UFormGroup>
             
-            
-            
-            <!-- Password -->
             <UFormGroup :label="$t('Password')" required>
-                <UInput id="email" class="my-4" type="password" nam
-                e="password" required autofocus autocomplete="current-password" />
+                <UInput id="email" class="my-4" type="password" name="password" required autofocus autocomplete="current-password" />
             </UFormGroup> 
 
-            <!-- Remember Me -->
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <UCheckbox id="remember_me" type="checkbox" class="rounded" name="remember"/>
@@ -35,28 +26,22 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                <UButton variant="link" href="">
                     {{ $t('Forgot your password?') }} 
-                </a>
+                </UButton>
 
                 <UButton class="ltr:ml-3 rtl:mr-3 text-xs py-2 px-4">
                         {{ $t('Log in') }} 
                 </UButton>
             </div>
         </UForm>
-    </authCard>
+    </AuthCard>
 </template>
+
 <script setup>
     definePageMeta({
         layout : 'guest'
     });
 </script>
-<style scoped>
-    .banner{
-        font-family : Freestyle Script;
-        font-size : 5rem;
-        text-align : center;
-        color : #3C1787;
-    }
-</style>
+
+<style scoped></style>
